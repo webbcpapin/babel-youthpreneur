601396fe13c94f97fcf8f28a3756f06ef568c4a4
agung3956 <agung3956@gmail.com>
2026-07-07 16:00:17 +0700

    fix: point monitoring to working registration backend

--- public/monitoring/config.js
+++ public/monitoring/config.js
@@ -1,6 +1,6 @@
 window.MONITORING_CONFIG = {
   sheetId: '1PqRraw7Qt5nfpWECAemnTRH4edrKDZfti0gBImmgDbI',
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycby8ht0rofift29FFnNzA4MOywQJXTlh50iiOPlfnAD0dLVL2EKu4hDZNU9yLRN4EoK4/exec',
-  supabaseUrl: 'https://ilvutqcznkdvtwlcvqzt.supabase.co',
-  supabaseAnonKey: 'sb_publishable_D_xpMW0HYRdoirAjF0s1Xw_g9EcseXA',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzM2GBdUbB7sDu5WM0AE3CRGkOISCKMX9ywn-8rUjpoFqO92KMoGpHlK4Q7tY7MuObW/exec',
+  supabaseUrl: '',
+  supabaseAnonKey: '',
 }
