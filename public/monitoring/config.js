187102b2152ddaa547684549742ad718c218598c
agung3956 <agung3956@gmail.com>
2026-07-21 13:00:36 +0700

    chore: use latest monitoring Apps Script deployment

--- public/monitoring/config.js
+++ public/monitoring/config.js
@@ -1,5 +1,5 @@
 window.MONITORING_CONFIG = {
   sheetId: '1PqRraw7Qt5nfpWECAemnTRH4edrKDZfti0gBImmgDbI',
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzA3u8SyX1UBgvJvEtSBy3IPyLq_2iySv9svPjcZSCJLUerC-R5FTgeEv9fDXtZTRuI/exec',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwoNp6SiFKT273bFV0skMltCdFCmGR6GEHTzTP8pLmDVaspm--V91RDSb0va2Z-HzzP/exec',
   googleOAuthClientId: '238923314877-2so8cu8infdh2eq0bvel0v3avent4f84.apps.googleusercontent.com',
 }
